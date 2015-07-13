b189ff17de536c5e7ee47878594922d0503fd2b7
Victor Queiroz <victorcqueirozg@gmail.com>
2015-07-12 22:26:22 -0300

    expectation database query implemented on mockConnection

--- helpers/mock-connection.js
+++ helpers/mock-connection.js
@@ -7,8 +7,17 @@ util.inherits(MockConnection, Connection);
 module.exports = MockConnection;
 
 function MockConnection () {
+	var queryExpectationList = this.queryExpectationList = [];
 	this.adapter = {
-		executeQuery: function () {
+		executeQuery: function (query) {
+			if(queryExpectationList.length > 0) {
+				assert.equal(first(queryExpectationList), query, 'unexpect database query');
+				
+				if(first(queryExpectationList) == query) {
+					queryExpectationList.splice(queryExpectationList.indexOf(query), 1);
+				}
+			}
+
 			return Q.Promise(function (resolve, reject) {
 				process.nextTick(function () {
 					resolve();
@@ -17,3 +26,9 @@ function MockConnection () {
 		}
 	};
 }
+
+extend(MockConnection.prototype, {
+	expectQuery: function (query) {
+		this.queryExpectationList.push(query);
+	}
+});
